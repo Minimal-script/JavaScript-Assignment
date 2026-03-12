169cebea820ef9ab6f03817c6bb557bfb0799c66
Minimal-script <arshch4747@gmail.com>
2026-03-12 11:38:32 +0500

    Added JavaScript

---
+++ script1.js
@@ -0,0 +1,8 @@
+var _myName= "Areesha";
+console.log(_myName)
+var myAge= "19";
+console.log(myAge)
+var myCity= "BHAKKAR";
+console.log(myCity)
+var favlanguage="JavaScript";
+console.log(favlanguage)
