1ac18da97e2ebda6473e1e2bca7e08d2db32a63c
Minimal-script <arshch4747@gmail.com>
2026-03-13 10:43:21 +0500

    Added script3.js file

---
+++ script3.js
@@ -0,0 +1,12 @@
+var age = 20;
+var hasID = true;
+console.log(age > 15 && hasID)
+
+let a = 4;
+let b = 3;
+console.log(a++ + ++b);
+console.log(a);
+console.log(b);
+
+let isLoggedIn = false;
+console.log(!isLoggedIn);
