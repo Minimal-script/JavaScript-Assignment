eed9a7c40a4c1b3c7726425e5ba0484cab874105
Minimal-script <arshch4747@gmail.com>
2026-03-15 13:34:52 +0500

    Added script3.js

--- script3.js
+++ script3.js
@@ -5,10 +5,9 @@ console.log(age > 15 && hasID)
 let a = 4;
 let b = 3;
 console.log(a++ + ++b);
-console.log(a);
+console.log(a); 
 console.log(b);
 
 let isLoggedIn = false;
-console.log(!isLoggedIn);
-
+console.log(!isLoggedIn)
 
